3c4041184406df20965f4716e401ffdae3e83c7e
TK <1009022110@QQ.COM>
2022-03-26 14:19:56 +0800

    excutors学习

--- executor/src/test/java/com/vostroi/executor/ExecutorApplicationTests.java
+++ executor/src/test/java/com/vostroi/executor/ExecutorApplicationTests.java
@@ -1,10 +1,7 @@
 package com.vostroi.executor;
 
-import com.vostroi.executor.concurrent.CallableTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.concurrent.*;
 
 @SpringBootTest
 class ExecutorApplicationTests {
